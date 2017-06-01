08c01629a70267fb27731a526cdbd646b324e0a6
chienbm <chien10a3s@gmail.com>
2017-06-01 00:20:05 -0700

    Chien Commit

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use App;
+use Cookie;
+use Response;
+use App\Shop;
+use App\Product;
+use App\Setting; 
+/**
+ * Class HomeController
+ * @package App\Http\Controllers
+ */
+ 
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function index()
+    {
+        die('dfsfsfs');
+    }
+}

8e75d4df5e371e76dd8937508177b95e2700e920
nkhoai <nkhoai.ict@gmail.com>
2017-06-01 21:49:35 +0700

    Start Landing HTML

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Kitchen</title>
+        <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/css/bootstrap-theme.min.css') }}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div id="page" class="site">
+            <div id="masthead" class="site-header fixed-header">
+                <div class="dgt-header-inner dgt-position-fixed">
+                    <div class="header-primary">
+                        <div class="container">
+                            <div class="header-section"> 
+                                <span class="header-mobile-open-icon visible-sm visible-xs">
+                                    <i class="ion-navicon"></i>
+                                </span>
+                                <div class="row">
+                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                        <div class="site-branding">
+                                            <p class="logo" id="logo">
+                                                <a href="{{url('/')}}" rel="home"> 
+                                                    <img src="{{ asset('/images/logo.png') }}" alt="" /> 
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="hidden-sm hidden-xs col-sm-6 col-md-6">
+                                        <form method="get" id="searchform" class="searchform" action="http://demo.kevthemes.com/teaux/">
+                                            <div class="dgt-search-form">
+                                                <div class="dgt-input-seach">
+                                                    <input type="text" placeholder="Type and hit enter" name="s" id="s" />
+                                                    <input type="hidden" value="post" name="post_type" id="posttype" />
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="hidden-sm hidden-xs dgt-header-right col-sm-2 col-md-2">
+                                        <div class="pull-left">
+                                            
+                                        </div>
+                                        <div class="pull-right">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('customer.index');
 });
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();

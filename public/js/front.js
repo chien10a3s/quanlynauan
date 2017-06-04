0cc0827f2f36fd0031bcaf3398073b08cbbc4494
nkhoai <nkhoai.ict@gmail.com>
2017-06-04 21:31:33 +0700

    Hoai commit

--- public/js/front.js
+++ public/js/front.js
@@ -14,5 +14,18 @@ $(document).ready(function(){
         autoplayHoverPause:true,
         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>']
     });
+    
+    //$('.header-primary').sticky({topSpacing:101});
+});
+
+$(window).scroll(function(){
+    if($(window).scrollTop() > 101){
+        $('#masthead').css('height', $('.header-primary').outerHeight());
+        $('body').addClass('header-fixed');
+        
+    }else{
+        $('#masthead').css('height', $('.header-primary').outerHeight());
+        $('body').removeClass('header-fixed');
+    }
 });
 
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -16,6 +16,7 @@
         <script type="text/javascript" src="{{ asset('/js/jquery-1.12.4.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('/js/owl.carousel.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('/js/jquery.sticky.js') }}"></script>
         <script type="text/javascript" src="{{ asset('/js/front.js') }}"></script>
     </head>
     <body>
@@ -29,7 +30,7 @@
                                     <i class="ion-navicon"></i>
                                 </span>
                                 <div class="row">
-                                    <div class="col-sm-3 col-md-3 col-lg-3">
+                                    <div class="col-sm-3 col-md-3 col-lg-2">
                                         <div class="site-branding">
                                             <p class="logo" id="logo">
                                                 <a href="{{url('/')}}" rel="home"> 
@@ -38,7 +39,8 @@
                                             </p>
                                         </div>
                                     </div>
-                                    <div class="hidden-sm hidden-xs col-sm-6 col-md-6">
+                                    <div class="hidden-sm hidden-xs col-sm-6 col-md-6 col-lg-7">
+                                        <!--
                                         <form method="get" id="searchform" class="searchform" action="http://demo.kevthemes.com/teaux/">
                                             <div class="dgt-search-form">
                                                 <div class="dgt-input-seach">
@@ -47,6 +49,16 @@
                                                 </div>
                                             </div>
                                         </form>
+                                        -->
+                                        <div class="main-menu">
+                                            <ul id="menu-main-menu-1" class="menu">
+                                                <li class="menu-item current-menu-item"><a href="#">Trang chủ</a></li>
+                                                <li class="menu-item"><a href="#">Hệ thống cửa hàng</a></li>
+                                                <li class="menu-item"><a href="#">Hướng dẫn đặt hàng</a></li>
+                                                <li class="menu-item"><a href="#">Hỗ trợ khách hàng</a></li>
+                                                <li class="menu-item"><a href="#">Liên hệ</a></li>
+                                            </ul>
+                                        </div>
                                     </div>
                                     <div class="hidden-sm hidden-xs dgt-header-right col-sm-3 col-md-3">
                                         <ul class="user-info">
@@ -58,17 +70,6 @@
                                             </li>
                                         </ul>
                                     </div>
-                                    <div class="col-xs-12">
-                                        <div class="main-menu">
-                                            <ul id="menu-main-menu-1" class="menu">
-                                                <li class="menu-item current-menu-item"><a href="#">Trang chủ</a></li>
-                                                <li class="menu-item"><a href="#">Hệ thống cửa hàng</a></li>
-                                                <li class="menu-item"><a href="#">Hướng dẫn đặt hàng</a></li>
-                                                <li class="menu-item"><a href="#">Hỗ trợ khách hàng</a></li>
-                                                <li class="menu-item"><a href="#">Liên hệ</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -76,25 +77,7 @@
                 </div>
             </div>
             <div id="slider-wrap" class="container">
-                <div class="row">
-                    <div class="col-md-9 col-xs-12">
-                        <div id="mainslider" class="owl-carousel">
-                            <div class="slide">
-                                <a href="#"><img src="{{ asset('/images/img-slider-1.jpg') }}" class="img-responsive" /></a>
-                            </div>
-                            <div class="slide">
-                                <a href="#"><img src="{{ asset('/images/img-slider-2.jpg') }}" class="img-responsive" /></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-xs-12">
-                        <ul id="slide-column-2">
-                            <li><a href="#"><img src="{{ asset('/images/small-1.jpg') }}" class="img-responsive" /></a></li>
-                            <li><a href="#"><img src="{{ asset('/images/small-2.jpg') }}" class="img-responsive" /></a></li>
-                            <li><a href="#"><img src="{{ asset('/images/small-3.jpg') }}" class="img-responsive" /></a></li>
-                        </ul>
-                    </div>
-                </div>
+                
                 <!--
                 <div class="image_thumb">
                     <ul>
@@ -129,6 +112,27 @@
             
             <div id="content">
                 <div class="container">
+                
+                    <div class="row">
+                        <div class="col-md-9 col-xs-12">
+                            <div id="mainslider" class="owl-carousel">
+                                <div class="slide">
+                                    <a href="#"><img src="{{ asset('/images/img-slider-1.jpg') }}" class="img-responsive" /></a>
+                                </div>
+                                <div class="slide">
+                                    <a href="#"><img src="{{ asset('/images/img-slider-2.jpg') }}" class="img-responsive" /></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-xs-12">
+                            <ul id="slide-column-2">
+                                <li><a href="#"><img src="{{ asset('/images/small-1.jpg') }}" class="img-responsive" /></a></li>
+                                <li><a href="#"><img src="{{ asset('/images/small-2.jpg') }}" class="img-responsive" /></a></li>
+                                <li><a href="#"><img src="{{ asset('/images/small-3.jpg') }}" class="img-responsive" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                
                     <div class="service">
             			<div class="col-xs-6 col-sm-3 service-item">
             				<a href="#">
@@ -175,44 +179,134 @@
             				</a>
             			</div>
             		</div>
-                </div>
-            </div>
-            <div id="section-2">
-                <div class="container">
-                    <div class="dgt-custom-heading">
-                        <span class="dgt-heading-icon">
-                            <img src="{{ asset('/images/icon-heading-small.png') }}" width="27" height="35" alt="Icon Heading"/>
-                        </span>
-                        <h2 style="text-align: center" class="dgt-heading">Teaux</h2>
-                        <p class="dgt-sub-title" style="color: #ffffff">Về chúng tôi</p>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-3 dgt-counter-box-inner"> 
-                            <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-pizza"></i></span>
-                            <h3 class="dgt-counter">25.540</h3>
-                            <h4>Khách hàng</h4>
+                    <div id="section-2">
+                        <div class="dgt-custom-heading">
+                            <span class="dgt-heading-icon">
+                                <img src="{{ asset('/images/icon-heading-small.png') }}" width="27" height="35" alt="Icon Heading"/>
+                            </span>
+                            <h2 style="text-align: center" class="dgt-heading">Teaux</h2>
+                            <p class="dgt-sub-title" style="color: #ffffff">Về chúng tôi</p>
                         </div>
-                        
-                        <div class="col-sm-3 dgt-counter-box-inner"> 
-                            <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-noodles"></i></span>
-                            <h3 class="dgt-counter">3.500</h3>
-                            <h4>Món ăn</h4>
-                        </div>
-                        
-                        <div class="col-sm-3 dgt-counter-box-inner"> 
-                            <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-chef"></i></span>
-                            <h3 class="dgt-counter">25.540</h3>
-                            <h4>Đầu bếp</h4>
-                        </div>
-                        
-                        <div class="col-sm-3 dgt-counter-box-inner"> 
-                            <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-sandwich"></i></span>
-                            <h3 class="dgt-counter">25.540</h3>
-                            <h4>Đánh giá</h4>
+                        <div class="row">
+                            <div class="col-sm-3 dgt-counter-box-inner"> 
+                                <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-pizza"></i></span>
+                                <h3 class="dgt-counter">25.540</h3>
+                                <h4>Khách hàng</h4>
+                            </div>
+                            
+                            <div class="col-sm-3 dgt-counter-box-inner"> 
+                                <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-noodles"></i></span>
+                                <h3 class="dgt-counter">3.500</h3>
+                                <h4>Món ăn</h4>
+                            </div>
+                            
+                            <div class="col-sm-3 dgt-counter-box-inner"> 
+                                <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-chef"></i></span>
+                                <h3 class="dgt-counter">25.540</h3>
+                                <h4>Đầu bếp</h4>
+                            </div>
+                            
+                            <div class="col-sm-3 dgt-counter-box-inner"> 
+                                <span class="dgt-icon"><i style="font-size: 80px" class="icofont icofont-sandwich"></i></span>
+                                <h3 class="dgt-counter">25.540</h3>
+                                <h4>Đánh giá</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
+            <footer id="footer" class="site-footer footer-fixed">
+                <div class="footer-widget">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-3 col-md-3">
+                                <div id="dgt_fiora_about_widget-3" class="widget first dgt_fiora_about_widget">
+                                    <div class="dgt-widget-about">
+                                        <div class="logo-footer">
+                                            <a href="#" rel="home"> <img src="{{ asset('/images/logo-footer.png') }}" alt=""></a>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                        <div class="dgt-about-info"></div>
+                                        
+                                        <div class="dgt-widget-about">
+                                            <div class="dgt-about-info">
+                                                <p><i class="icofont icofont-location-pin"></i>Keangnam Landmark Tower, Ha Noi</p>
+                                                <p><i class="icofont icofont-mail"></i><a href="mailto:support@company.com">support@company.com</a>
+                                                </p>
+                                                <p><i class="icofont icofont-ui-touch-phone"></i>(0123) 456-789-000</p>
+                                                <p><i class="icofont icofont-clock-time"></i>Monday - Sunday: 8:00 AM - 22:00 PM</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-3 col-md-3">
+                                <div id="dgt_soraka_latest_news_widget-6" class="widget dgt_soraka_latest_news_widget">
+                                    <h3 class="widget-title">Latest Posts</h3>
+                                    <div class="dgt-blog-sidebar">
+                                        <div class="dgt-blog-sidebar-item clearfix">
+                                            <div class="blog-image">
+                                                <a href="http://demo.kevthemes.com/teaux/restaurant-to-host-spring/" rel="bookmark" title="Permanent Link: Restaurant to host “Spring”"><img width="230" height="230" src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-1-230x230.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+                                                </a>
+                                            </div>
+                                            <div class="blog-info">
+                                                <h4><a href="http://demo.kevthemes.com/teaux/restaurant-to-host-spring/" rel="bookmark" title="Permanent Link: Restaurant to host “Spring”">Restaurant to host “Spring”</a></h4> <span class="dgt-blog-date">Oct 31, 2016</span>
+                                            </div>
+                                        </div>
+                                        <div class="dgt-blog-sidebar-item clearfix">
+                                            <div class="blog-image">
+                                                <a href="http://demo.kevthemes.com/teaux/join-us-for-le-soild-restaurant/" rel="bookmark" title="Permanent Link: Join us for Le Soild Restaurant"><img width="230" height="230" src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-2-230x230.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+                                                </a>
+                                            </div>
+                                            <div class="blog-info">
+                                                <h4><a href="http://demo.kevthemes.com/teaux/join-us-for-le-soild-restaurant/" rel="bookmark" title="Permanent Link: Join us for Le Soild Restaurant">Join us for Le Soild Restaurant</a></h4> <span class="dgt-blog-date">Oct 31, 2016</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-3 col-md-3">
+                                <h3 class="widget-title">Thông tin</h3>
+                                    <div class="dgt-blog-sidebar">
+                                        <ul class="care-customer">
+            								<li><a href="#">Giới thiệu</a></li>
+            								<li><a href="#">Điều khoản sử dụng</a></li>
+            								<li><a href="#">Chính sách bảo mật thông tin</a></li>
+            								<li><a href="#">Quy chế Quản lý Hoạt động</a></li>
+            								
+            							</ul>
+                                    </div>
+                            </div>
+                            <div class="col-sm-3 col-md-3">
+                                <div class="dgt-social icon-show name-hidden">
+                                    <ul>
+                                        <li class="twitter"><a title="Twitter" href="#"><span>Twitter</span><i class="ion-social-twitter"></i></a>
+                                        </li>
+                                        <li class="facebook"><a title="Facebook" href="#"><span>Facebook</span><i class="ion-social-facebook"></i></a>
+                                        </li>
+                                        <li class="Instagram"><a title="Tumblr" href="#"><span>Instagram</span><i class="ion-social-instagram"></i></a>
+                                        </li>
+                                        <li class="youtube"><a title="Youtube" href="#"><span>Youtube</span><i class="ion-social-youtube-outline"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="coppyright">
+                    <div class="container">
+                        <div class="coppyright-inner dgt-al-center">
+                            <div class="row">
+                                <p>© Copyright 2017, All Rights Reserved</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            
         </div>
     </body>
 </html>

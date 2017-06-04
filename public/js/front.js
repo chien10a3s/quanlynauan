74be1e6474d932e9223447c3de605700b9e6a613
nkhoai <nkhoai.ict@gmail.com>
2017-06-04 23:28:11 +0700

    Hoai commit

--- public/js/front.js
+++ public/js/front.js
@@ -15,6 +15,22 @@ $(document).ready(function(){
         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>']
     });
     
+    $('#testimonial').owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items:1,
+        loop: true,
+        margin:0,
+        stagePadding:0,
+        smartSpeed:450,
+        nav: true,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>']
+    });
+    
     //$('.header-primary').sticky({topSpacing:101});
 });
 
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -20,6 +20,15 @@
         <script type="text/javascript" src="{{ asset('/js/front.js') }}"></script>
     </head>
     <body>
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.9&appId=1713794735515717";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+    
         <div id="page" class="site">
             <div id="masthead" class="site-header fixed-header">
                 <div class="dgt-header-inner dgt-position-fixed">
@@ -213,6 +222,127 @@
                             </div>
                         </div>
                     </div>
+                    
+                    <div id="blog" class="vc_row wpb_row vc_row-fluid dgt-bg-inherit">
+                        <div class="wpb_column vc_column_container vc_col-sm-12">
+                            <div class="vc_column-inner ">
+                                <div class="wpb_wrapper">
+                                    <div class="dgt-custom-heading vc_custom_heading dgt-al-center">
+                                        <span class="dgt-heading-icon">
+                                        <img src="http://demo.kevthemes.com/teaux/wp-content/uploads/2017/03/icon-heading.png" width="27" height="35" alt="Icon Heading"></span>
+                                        <h2 style="text-align: center" class="dgt-heading">Our blog</h2>
+                                        <p class="dgt-sub-title">for cooking-holic</p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="dgt-vc-element dgt-blog-grid">
+                                        <div class="row">
+                                            <div class="dgt-blog-item  dgt-clear-left col-sm-4 col-md-4 tips">
+                                                <div class="dgt-blog-item-inner">
+                                                    <div class="post-feature-image">
+                                                        <div class="wrapper-img">
+                                                            <a href="http://demo.kevthemes.com/teaux/restaurant-to-host-spring/" title="Restaurant to host “Spring”"> <img width="660" height="440" src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-1.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-1.jpg 700w, http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-1-300x200.jpg 300w" sizes="(max-width: 660px) 100vw, 660px"> </a>
+                                                        </div> <span class="dgt-blog-date">31<span>Oct</span></span>
+                                                    </div>
+                                                    <div class="dgt-blog-info">
+                                                        <h4 class="dgt-blog-title"> <a class="blog-title" href="http://demo.kevthemes.com/teaux/restaurant-to-host-spring/" title="Restaurant to host “Spring”"> Restaurant to host “Spring” </a></h4>
+                                                        <div class="info-post clearfix"> <span class="dgt-blog-author"><img src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/11/avatar-2.png" width="20" height="20" alt="Restaurant" class="avatar avatar-20 wp-user-avatar wp-user-avatar-20 alignnone photo"> <a href="http://demo.kevthemes.com/teaux/author/admin/" title="Posts by Restaurant" rel="author">Restaurant</a></span>
+                                                        </div>
+                                                        <div class="dgt-blog-description"> English has paired 16 Memphis chefs with 16 guest chefs from throughout the country to create a one-of-a-kind culinary...</div>
+                                                        <div class="clearfix"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="dgt-blog-item  col-sm-4 col-md-4 blog">
+                                                <div class="dgt-blog-item-inner">
+                                                    <div class="post-feature-image">
+                                                        <div class="wrapper-img">
+                                                            <a href="http://demo.kevthemes.com/teaux/join-us-for-le-soild-restaurant/" title="Join us for Le Soild Restaurant"> <img width="660" height="440" src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-2.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-2.jpg 700w, http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-2-300x200.jpg 300w" sizes="(max-width: 660px) 100vw, 660px"> </a>
+                                                        </div> <span class="dgt-blog-date">31<span>Oct</span></span>
+                                                    </div>
+                                                    <div class="dgt-blog-info">
+                                                        <h4 class="dgt-blog-title"> <a class="blog-title" href="http://demo.kevthemes.com/teaux/join-us-for-le-soild-restaurant/" title="Join us for Le Soild Restaurant"> Join us for Le Soild Restaurant </a></h4>
+                                                        <div class="info-post clearfix"> <span class="dgt-blog-author"><img src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/11/avatar-2.png" width="20" height="20" alt="Restaurant" class="avatar avatar-20 wp-user-avatar wp-user-avatar-20 alignnone photo"> <a href="http://demo.kevthemes.com/teaux/author/admin/" title="Posts by Restaurant" rel="author">Restaurant</a></span>
+                                                        </div>
+                                                        <div class="dgt-blog-description"> English has paired 16 Memphis chefs with 16 guest chefs from throughout the country to create a one-of-a-kind culinary...</div>
+                                                        <div class="clearfix"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="dgt-blog-item  col-sm-4 col-md-4 blog">
+                                                <div class="dgt-blog-item-inner">
+                                                    <div class="post-feature-image">
+                                                        <div class="wrapper-img">
+                                                            <a href="http://demo.kevthemes.com/teaux/yelp-restaurant-wins-best-fine-dining/" title="YELP: Restaurant wins Best Fine"> <img width="660" height="440" src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-3.jpg" class="attachment-large size-large wp-post-image" alt="" srcset="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-3.jpg 700w, http://demo.kevthemes.com/teaux/wp-content/uploads/2016/10/img-blog-3-300x200.jpg 300w" sizes="(max-width: 660px) 100vw, 660px"> </a>
+                                                        </div> <span class="dgt-blog-date">31<span>Oct</span></span>
+                                                    </div>
+                                                    <div class="dgt-blog-info">
+                                                        <h4 class="dgt-blog-title"> <a class="blog-title" href="http://demo.kevthemes.com/teaux/yelp-restaurant-wins-best-fine-dining/" title="YELP: Restaurant wins Best Fine"> YELP: Restaurant wins Best Fine </a></h4>
+                                                        <div class="info-post clearfix"> <span class="dgt-blog-author"><img src="http://demo.kevthemes.com/teaux/wp-content/uploads/2016/11/avatar-2.png" width="20" height="20" alt="Restaurant" class="avatar avatar-20 wp-user-avatar wp-user-avatar-20 alignnone photo"> <a href="http://demo.kevthemes.com/teaux/author/admin/" title="Posts by Restaurant" rel="author">Restaurant</a></span>
+                                                        </div>
+                                                        <div class="dgt-blog-description"> English has paired 16 Memphis chefs with 16 guest chefs from throughout the country to create a one-of-a-kind culinary...</div>
+                                                        <div class="clearfix"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div id="testimonial" class="owl-carousel">
+                        <div class="dgt-testimonial-item dgt-al-center">
+                            <div class="dgt-testimonial-avatar"> 
+                                <img width="180" height="180" src="{{ asset('/images/avatar-1.png') }}" class="attachment-full size-full img-responsive" alt="">
+                            </div>
+                            <div class="dgt-testimonial-info">
+                                <p style="color: #ffffff">Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu</p>
+                                <h3 style="color: #ffffff">NK Hoài</h3>
+                                <h4>Types Of Cookware Pots And Pans</h4>
+                            </div>
+                        </div>
+                        <div class="dgt-testimonial-item dgt-al-center">
+                            <div class="dgt-testimonial-avatar"> 
+                                <img width="180" height="180" src="{{ asset('/images/avatar-2.png') }}" class="attachment-full size-full img-responsive" alt="">
+                            </div>
+                            <div class="dgt-testimonial-info">
+                                <p style="color: #ffffff">Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu</p>
+                                <h3 style="color: #ffffff">NK Hoài</h3>
+                                <h4>Types Of Cookware Pots And Pans</h4>
+                            </div>
+                        </div>
+                        <div class="dgt-testimonial-item dgt-al-center">
+                            <div class="dgt-testimonial-avatar"> 
+                                <img width="180" height="180" src="{{ asset('/images/avatar-19.jpg') }}" class="attachment-full size-full img-responsive" alt="">
+                            </div>
+                            <div class="dgt-testimonial-info">
+                                <p style="color: #ffffff">Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu</p>
+                                <h3 style="color: #ffffff">NK Hoài</h3>
+                                <h4>Types Of Cookware Pots And Pans</h4>
+                            </div>
+                        </div><div class="dgt-testimonial-item dgt-al-center">
+                            <div class="dgt-testimonial-avatar"> 
+                                <img width="180" height="180" src="{{ asset('/images/avatar-20.jpg') }}" class="attachment-full size-full img-responsive" alt="">
+                            </div>
+                            <div class="dgt-testimonial-info">
+                                <p style="color: #ffffff">Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu</p>
+                                <h3 style="color: #ffffff">NK Hoài</h3>
+                                <h4>Types Of Cookware Pots And Pans</h4>
+                            </div>
+                        </div>
+                        <div class="dgt-testimonial-item dgt-al-center">
+                            <div class="dgt-testimonial-avatar"> 
+                                <img width="180" height="180" src="{{ asset('/images/avatar-1.png') }}" class="attachment-full size-full img-responsive" alt="">
+                            </div>
+                            <div class="dgt-testimonial-info">
+                                <p style="color: #ffffff">Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu. Đồ ăn ngon, rất hợp khẩu vị. Nấu đúng yêu cầu</p>
+                                <h3 style="color: #ffffff">NK Hoài</h3>
+                                <h4>Types Of Cookware Pots And Pans</h4>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
             
@@ -243,7 +373,7 @@
                             </div>
                             <div class="col-sm-3 col-md-3">
                                 <div id="dgt_soraka_latest_news_widget-6" class="widget dgt_soraka_latest_news_widget">
-                                    <h3 class="widget-title">Latest Posts</h3>
+                                    <h3 class="widget-title">BÀI VIẾT MỚI NHẤT</h3>
                                     <div class="dgt-blog-sidebar">
                                         <div class="dgt-blog-sidebar-item clearfix">
                                             <div class="blog-image">
@@ -267,29 +397,22 @@
                                 </div>
                             </div>
                             <div class="col-sm-3 col-md-3">
-                                <h3 class="widget-title">Thông tin</h3>
-                                    <div class="dgt-blog-sidebar">
-                                        <ul class="care-customer">
-            								<li><a href="#">Giới thiệu</a></li>
-            								<li><a href="#">Điều khoản sử dụng</a></li>
-            								<li><a href="#">Chính sách bảo mật thông tin</a></li>
-            								<li><a href="#">Quy chế Quản lý Hoạt động</a></li>
-            								
-            							</ul>
-                                    </div>
+                                <h3 class="widget-title">THÔNG TIN</h3>
+                                <div class="dgt-blog-sidebar">
+                                    <ul class="care-customer">
+        								<li><a href="#">Giới thiệu</a></li>
+        								<li><a href="#">Điều khoản sử dụng</a></li>
+        								<li><a href="#">Chính sách bảo mật thông tin</a></li>
+        								<li><a href="#">Hướng dẫn đặt hàng</a></li>
+        								<li><a href="#">Hệ thống của hàng</a></li>
+                                        <li><a href="#">Liên hệ</a></li>
+        							</ul>
+                                </div>
                             </div>
                             <div class="col-sm-3 col-md-3">
+                                <h3 class="widget-title">LIÊN HỆ</h3>
                                 <div class="dgt-social icon-show name-hidden">
-                                    <ul>
-                                        <li class="twitter"><a title="Twitter" href="#"><span>Twitter</span><i class="ion-social-twitter"></i></a>
-                                        </li>
-                                        <li class="facebook"><a title="Facebook" href="#"><span>Facebook</span><i class="ion-social-facebook"></i></a>
-                                        </li>
-                                        <li class="Instagram"><a title="Tumblr" href="#"><span>Instagram</span><i class="ion-social-instagram"></i></a>
-                                        </li>
-                                        <li class="youtube"><a title="Youtube" href="#"><span>Youtube</span><i class="ion-social-youtube-outline"></i></a>
-                                        </li>
-                                    </ul>
+                                    <div class="fb-page" data-href="https://www.facebook.com/facebook" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
                                 </div>
                             </div>
                         </div>
